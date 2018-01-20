3cdf6f12fb14060a2f5d1ea0622c8b45e6641314
mzaini30 <muhzaini30@gmail.com>
2018-01-20 01:20:46 +0000

    oke

--- bin/infinity.js
+++ bin/infinity.js
@@ -5,26 +5,4 @@ jQuery(function($){
 		hideNav: '.pagination',
 		debug: true
 	});
-
-	var nextURL;
-
-	function updateNextURL( doc ) {
-	  nextURL = $( doc ).find('.pagination__next').attr('href');
-	}
-	// get initial nextURL
-	updateNextURL( document );
-
-	// init Infinite Scroll
-	var $container = $('.infinity').infiniteScroll({
-	  // use function to set custom URLs
-	  path: function() {
-	    return nextURL;
-	  },
-	});
-
-	// update nextURL on page load
-	$container.on( 'load.infiniteScroll', function( event, response ) {
-	  updateNextURL( response );
-	});
-
 });
