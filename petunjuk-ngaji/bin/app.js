da43fa122826ab30e18fff5bf295ecde59fc5e0c
mzaini30 <muhzaini30@gmail.com>
2018-05-18 14:37:15 +0000

    oke

--- petunjuk-ngaji/bin/app.js
+++ petunjuk-ngaji/bin/app.js
@@ -1,3 +1,21 @@
+// init
+
 ambil_data = function(){
    return localStorage.getItem('petunjuk-ngaji')
+}
+tentukan_data = function(x){
+   return localStorage.setItem('petunjuk-ngaji', x)
+}
+
+// main data
+
+$('h1').html(ambil_data())
+$('input').attr({
+   placeholder: function(){
+      ambil_data()
+   }
+})
+
+data_baru = function(x){
+   tentukan_data(x)
 }
