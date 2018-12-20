c6aa15d281f1f96be06681964d785521ea50502b
mzaini30 <muhzaini30@gmail.com>
2018-12-20 18:38:17 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
+kategori = 'soul'
+
+$('.' + kategori).each(function(){
+	$(this).css('display', 'block')
+	$(this).find('img').attr('src', $(this).find('img').data('src'))
+})
+
 $('.theia').theiaStickySidebar({
 	additionalMarginTop: 20
 })
