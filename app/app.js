695f55e35827a8aedd252169d62a45cfdd373822
mzaini30 <muhzaini30@gmail.com>
2018-12-20 20:27:36 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -1,4 +1,32 @@
-kategori = 'soul'
+function mode(array)
+{
+    if(array.length == 0)
+        return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+        var el = array[i];
+        if(modeMap[el] == null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;  
+        if(modeMap[el] > maxCount)
+        {
+            maxEl = el;
+            maxCount = modeMap[el];
+        }
+    }
+    return maxEl;
+}
+
+// kategori --> replace spasi jadi -
+
+kategori = mode(JSON.parse(localStorage.getItem('kategori blog')))
+
+if (kategori === null){
+	$('.bagian-minat').hide()
+}
 
 $('.' + kategori).each(function(n){
 	if (n < 10){
@@ -13,6 +41,19 @@ $('.' + kategori + '-sidebar').each(function(n){
 		$(this).find('img').attr('src', $(this).find('img').data('src'))
 	}
 })
+
+data_kunjungan = localStorage.getItem('kunjungan blog')
+localStorage.setItem('kunjungan blog', parseInt(data_kunjungan) + 1)
+
+total_kunjungan = 0
+if (parseInt(localStorage.getItem('kunjungan blog')) > 100){
+	total_kunjungan = '100+'
+} else {
+	total_kunjungan = localStorage.getItem('kunjungan blog')
+}
+
+$('.jumlah-kunjungan').html(total_kunjungan)
+$('.tampil-minat').html(kategori)
 
 $('.theia').theiaStickySidebar({
 	additionalMarginTop: 70
