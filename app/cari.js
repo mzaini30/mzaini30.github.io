f40034645bc64a11991091093fecf54a1d282089
mzaini30 <muhzaini30@gmail.com>
2019-02-28 13:13:08 +0800

    oke

--- app/cari.js
+++ app/cari.js
@@ -1,6 +1,14 @@
-$(".input_cari").on("keyup", function() {
+input_cari = ".input_cari"
+output_cari = ".output_cari .list-group-item"
+
+$(output_cari).hide()
+$(input_cari).on("keyup", function() {
+	// $('title').html($(this).val().length)
 	var value = $(this).val().toLowerCase();
-	$(".output_cari .list-group-item").filter(function() {
+	$(output_cari).filter(function() {
 		$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
 	});
+	if ($(this).val().length == 0){
+		$(output_cari).hide()
+	}
 });
