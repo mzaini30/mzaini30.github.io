e6118d743f547d85886972903ca3143f691ac32c
mzaini30 <muhzaini30@gmail.com>
2019-03-06 20:20:29 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -7,5 +7,3 @@ for (x = 1; x <= 5; x++){
 	$('.jelajah .list-group-item:nth-child('+ x +')').css('display', 'block')	
 }
 $('.jelajah2 .element:nth-child(1)').css('display', 'block')
-
-$('.user-card .header').css('background-image', 'url("https://cdn-images-1.medium.com/max/2000/1*19LWzzJJDPqGq67Vyd0EjQ.jpeg")')
