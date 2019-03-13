d0cc79beef7b4738fa0780a712ed8a09116229b0
mzaini30 <muhzaini30@gmail.com>
2019-03-14 04:33:16 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ $('.theia').theiaStickySidebar({
 	additionalMarginTop: 50,
 	additionalMarginBottom: 0
 })
+$('.theia_2').theiaStickySidebar({
+	additionalMarginTop: 70,
+	additionalMarginBottom: 0
+})
  
 $('.tulisan_lainnya a').each(function(){
 	if ($(this).attr('href') == ''){
