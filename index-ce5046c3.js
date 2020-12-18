db9e014da1c296a1b7095e9385a357e47f2a4c9b
mzaini30 <mzaini30@users.noreply.github.com>
2020-12-18 07:47:39 +0000

    deploy: e2c8b6fe07c779b9863032cddb555fa8bcf3ccd6

---
+++ index-ce5046c3.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as a}from"./main-19c1692e.js";export default class extends s{constructor(s){super(),t(this,s,null,null,a,{})}}
---
+++ main.js
@@ -0,0 +1,1 @@
+export{a as default}from"./main-19c1692e.js";
