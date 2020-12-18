54d3624ac1790381117efc03f2e2e0dbd2780a5d
mzaini30 <muhzaini30@gmail.com>
2020-12-19 07:19:03 +0800

    upload

--- src/pages/[slug].svelte
+++ src/pages/[slug].svelte
@@ -1,12 +1,38 @@
 <script>
- export let slug
+	export let slug
+	import marked from "marked"
+	import {highlight} from "highlight.js"
+	marked.setOptions({
+		breaks: true,
+		highlight: function(code, lang){
+			if (lang == ""){
+				lang = "javascript"
+			}
+			return highlight(lang, code).value
+		}
+	})
+	let data = {
+		judul: "Hello World",
+		tanggal: "2020-12-12 12:12:12",
+		isi: ` Hai... _halo_
+hai
+
+> Quote
+
+\`\`\`
+halo = async () => {
+	await alert("hai")
+}
+\`\`\`
+
+halo`
+	}
 </script>
-<h1>Heloo World</h1>
-<p><em>2020-08-08 12:55:00</em></p>
-<div class="isi">
- <p>lorem</p>
- <p>Halo</p>
-</div>
+{#if data}
+	<h1>{data.judul}</h1>
+	<p><em>{data.tanggal}</em></p>
+	<div class="isi">{@html marked(data.isi)}</div>
+{/if}
 <svelte:head>
- <title>Hello World</title>
+	<title>Hello World</title>
 </svelte:head>
