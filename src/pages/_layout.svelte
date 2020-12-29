e409e897cf11e960532645ecc93049d16b538467
mzaini30 <muhzaini30@gmail.com>
2020-12-29 10:47:41 +0800

    oke

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,6 +1,6 @@
 <div class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
  <div class="container">
-  <a class="navbar-brand" href="/">Zen</a>
+  <a class="navbar-brand" href="/">Muhammad Zaini, S.Psi</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" on:click={() => menu = !menu}>
    <span class="navbar-toggler-icon"></span>
   </button>
