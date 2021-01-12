d806b1495638d2d240bd1f6805e586c845fabcbd
mzaini30 <mzaini30@users.noreply.github.com>
2021-01-12 16:31:41 +0000

    deploy: e7bc88bb0fcd39b858dfa1605d5cdda100bb99fc

--- api-80ecb004.js
+++
@@ -1,1 +0,0 @@
-const a="97d551b191b15e8fd7f685d862796ae8",s="e247147cf8d338c89b9c4e4e361c2e3c",e="f6aa6952dbfcf2d78f6a1d78413e0283",c="https://mzaini30.nasihosting.com/database/sql.php";export{s as a,a as b,e as k,c as s};
