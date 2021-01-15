17560f47e751d163155d31c98f33f4f9aab9808c
mzaini30 <muhzaini30@gmail.com>
2021-01-15 14:25:11 +0800

    oke

--- src/iklan.js
+++ src/iklan.js
@@ -10,4 +10,23 @@ export const iklan = [
 	`<a href="https://www.domainesia.com/?aff=8297" target="_blank"><img src="https://dnva.me/f75bf" alt="www.domainesia.com"></a>`,
 	`<a href="https://www.domainesia.com/?aff=8297" target="_blank"><img src="https://dnva.me/3194b" alt="DomaiNesia"></a>`,
 	`<a href="https://www.domainesia.com/?aff=8297" target="_blank"><img src="https://dnva.me/17e58" alt="DomaiNesia"></a>`
+/*
+
+
+
+
+	
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://panel.niagahoster.co.id/banners/Set3-niagahoster-728x90.png" alt="Hosting Unlimited Indonesia" border="0" width="728" height="90" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://panel.niagahoster.co.id/banners/Set3-niagahoster-300x250.png" alt="Hosting Unlimited Indonesia" border="0" width="300" height="250" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://panel.niagahoster.co.id/banners/Set3-niagahoster-600x400.png" alt="Hosting Unlimited Indonesia" border="0" width="600" height="400" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/ads-persona-offline-to-online-business-cloud-hosting-affiliate-728-x-90.png" alt="Cloud Hosting Indonesia" border="0" width="728" height="90" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/ads-persona-offline-to-online-business-cloud-hosting-affiliate-300-x-250.png" alt="Cloud Hosting Indonesia" border="0" width="300" height="250" /></a>`,
+	` <a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/ads-persona-offline-to-online-business-cloud-hosting-affiliate-336-x-280.png" alt="Cloud Hosting Indonesia" border="0" width="160" height="600" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/affiliate-728-x-90.png" alt="PHP Dev Cloud Hosting" border="0" width="728" height="90" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/affiliate-300-x-250.png" alt="PHP Dev Cloud Hosting" border="0" width="300" height="250" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/affiliate-300-x-250.png" alt="PHP Dev Cloud Hosting" border="0" width="300" height="250" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/728-90-affiliate-starting-bisnis-online.png" alt="Affiliate Banner Unlimited Hosting Indonesia" border="0" width="728" height="90" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/300x250-affiliate-starting-bisnis-online.png" alt="Affiliate Banner Unlimited Hosting Indonesia" border="0" width="300" height="250" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img src="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/336x280-affiliate-starting-bisnis-online.png" alt="Affiliate Banner Unlimited Hosting Indonesia" border="0" width="160" height="600" /></a>`,
+	`<a href="https://panel.niagahoster.co.id/ref/420559" target="_blank"><img */
 ]
