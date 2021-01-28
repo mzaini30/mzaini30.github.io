b2df8619a3933fd914cecd74ec513e9e2cf737a8
mzaini30 <muhzaini30@gmail.com>
2021-01-28 12:03:45 +0800

    oke

--- script/data.js
+++ script/data.js
@@ -1,2 +1,2 @@
 exports.blog = "97d551b191b15e8fd7f685d862796ae8" // id, slug, judul, tanggal, isi, label
-exports.sql = "https://saitama.my.id/database/sql2.php"
+exports.sql = "https://database.saitama.my.id/sql2.php"
