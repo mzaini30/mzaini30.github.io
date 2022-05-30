a545ccb6522cd63f2194a30a182c7a00e969cad3
Zen <muhzaini30@gmail.com>
2022-05-31 06:26:33 +0800

    Tambahkan read more

--- src/pages/post.vue
+++ src/pages/post.vue
@@ -2,6 +2,9 @@
 	import Kembali from '/src/icon/kembali.vue'
 	import {onMounted} from 'vue'
 	import {Head} from '@vueuse/head'
+	import {acak} from 'kumpulan-tools'
+	import postingan from '/src/data/postingan.yml'
+	import {useRoute} from 'vue-router'
 
 	onMounted(() => {
 		document.title = document.querySelector('h1').innerText
@@ -15,6 +18,12 @@
 	<div class="p-5 prose konten mx-auto">
 		<router-link to="/" class='text-sm !no-underline mb-3 block !font-bold uppercase'><Kembali class='inline-block w-5 h-auto'></Kembali> Kembali ke beranda</router-link>
 		<router-view></router-view>
+		<h2>Tulisan Lainnya</h2>
+		<ol>
+			<li v-for='x in acak(postingan).filter(x => x.link != useRoute().path).filter((x, n) => n < 5)'>
+				<router-link :to='x.link'>{{ x.judul }}</router-link>
+			</li>
+		</ol>
 	</div>
 </template>
 
@@ -26,4 +35,5 @@
 	.konten.prose :where(:not(pre) > code):not(.not-prose)::after {
     content: "";
 	}
+	li:empty {display: none;}
 </style>
