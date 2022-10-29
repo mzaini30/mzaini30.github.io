8f4b19f3d6d1ad768380289e086b0e53de2f6ef1
Zen <muhzaini30@gmail.com>
2022-10-29 15:53:06 +0800

    Oke

--- src/icon/kembali.vue
+++
@@ -1,4 +0,0 @@
-
-<template>
-  <svg viewBox="0 0 16 16"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m7.25 3.75l-4.5 4.5l4.5 4.5m6-4.5H2.75"></path></svg>
-</template>
--- src/pages/post/funding.md
+++
@@ -1,8 +0,0 @@
-# Contoh Link Funding di Github
-
-```yaml
-ko_fi: mzaini30
-custom: ['https://www.nihbuatjajan.com/mzaini30']
-```
-
-Nah, itu diisi di `.github/FUNDING.yml`
